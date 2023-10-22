2e0219b0ee51f8052544d7ed21211e6cb777b82f
tuitenrein <levu1409@gmail.com>
2023-10-22 19:04:26 +0700

    Update NTPClientTester.ino

--- NTPClientTester/NTPClientTester.ino
+++ NTPClientTester/NTPClientTester.ino
@@ -1,7 +1,7 @@
 #include <NTPClient.h>
 // change next line to use with another board/shield
 #include <ESP8266WiFi.h>
-#include <WiFi.h> // for WiFi shield
+// #include <WiFi.h> // for WiFi shield
 //#include <WiFi101.h> // for WiFi 101 shield or MKR1000
 #include <WiFiUdp.h>
 
@@ -14,7 +14,7 @@ WiFiUDP ntpUDP;
 // changed later with setTimeOffset() ). Additionally you can specify the
 // update interval (in milliseconds, can be changed using setUpdateInterval() ).
 NTPClient timeClient(ntpUDP, "vn.pool.ntp.org", 25200, 60000); // ntpUDP, "VN", UTC7(in second), time update interval
-char weekDay [7][12] = {"Monday", "Tuesday", "Wednesday", "Thursday"};
+char weekDay [7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 void setup(){
   Serial.begin(9600); // 9600 baud
@@ -31,7 +31,8 @@ void setup(){
 
 void loop() {
   timeClient.update();
-
+  Serial.print(weekDay[timeClient.getDay()]);
+  Serial.print(" ");
   Serial.println(timeClient.getFormattedTime());
 
   delay(1000);
