296a2f8c609023bbe6fb55c645849e1d33494b2c
tuitenrein <levu1409@gmail.com>
2023-10-22 18:53:58 +0700

    Create NTPClientTester.ino

---
+++ NTPClientTester/NTPClientTester.ino
@@ -0,0 +1,38 @@
+#include <NTPClient.h>
+// change next line to use with another board/shield
+#include <ESP8266WiFi.h>
+#include <WiFi.h> // for WiFi shield
+//#include <WiFi101.h> // for WiFi 101 shield or MKR1000
+#include <WiFiUdp.h>
+
+const char *ssid     = "FixThatBug.vice_versa";
+const char *password = "nofreewifi";
+
+WiFiUDP ntpUDP;
+
+// You can specify the time server pool and the offset (in seconds, can be
+// changed later with setTimeOffset() ). Additionally you can specify the
+// update interval (in milliseconds, can be changed using setUpdateInterval() ).
+NTPClient timeClient(ntpUDP, "vn.pool.ntp.org", 25200, 60000); // ntpUDP, "VN", UTC7(in second), time update interval
+char weekDay [7][12] = {"Monday", "Tuesday", "Wednesday", "Thursday"};
+
+void setup(){
+  Serial.begin(9600); // 9600 baud
+
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  timeClient.begin();
+}
+
+void loop() {
+  timeClient.update();
+
+  Serial.println(timeClient.getFormattedTime());
+
+  delay(1000);
+}
