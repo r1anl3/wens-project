5a6a7edc31c95f6c844f6429bffc7d60318f0c80
tuitenrein <levu1409@gmail.com>
2023-11-08 19:09:18 +0700

    Create MQ135Tester.ino

---
+++ MQ135Tester/MQ135Tester.ino
@@ -0,0 +1,33 @@
+#include "MQ135.h"
+ 
+#define PIN_MQ135 A0
+MQ135 mq135_sensor = MQ135(PIN_MQ135);
+ 
+float temperature = 25.0;
+float humidity = 60.0;
+ 
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  float rzero = mq135_sensor.getRZero();
+  float correctedRZero = mq135_sensor.getCorrectedRZero(temperature, humidity);
+  float resistance = mq135_sensor.getResistance();
+  float ppm = mq135_sensor.getPPM();
+  float correctedPPM = mq135_sensor.getCorrectedPPM(temperature, humidity);
+ 
+  Serial.print("MQ135 RZero: ");
+  Serial.print(rzero);
+  Serial.print("\t Corrected RZero: ");
+  Serial.print(correctedRZero);
+  Serial.print("\t Resistance: ");
+  Serial.print(resistance);
+  Serial.print("\t PPM: ");
+  Serial.print(ppm);
+  Serial.print("\t Corrected PPM: ");
+  Serial.print(correctedPPM);
+  Serial.println("ppm");
+ 
+  delay(500);
+}
