9be1c0e7fb1ff6a067c9193fb31196837c39ef98
tuitenrein <levu1409@gmail.com>
2023-11-08 20:04:51 +0700

    Complete scenario

--- main/main.ino
+++ main/main.ino
@@ -9,66 +9,65 @@
 #include <WiFiUdp.h>
 #include "DHT.h"
 #include <BlynkSimpleEsp8266.h>
+#include "MQ135.h"
 
 #define DHTPIN 4 // SDA/D2
 #define DHTTYPE DHT11   // DHT 11
+#define PIN_MQ135 A0 // MQ135
 
 BlynkTimer timer;
-
-const char *ssid     = "FixThatBug.vice_versa";
-const char *password = "nofreewifi";
+WiFiUDP ntpUDP;
+DHT dht(DHTPIN, DHTTYPE);
+MQ135 mq135_sensor = MQ135(PIN_MQ135);
+float humid, temp, hic; // Values from DHT11
+float correctedRZero, resistance, correctedPPM; // Values from MQ135 
+const char *ssid     = "FixThatBug.vice_versa"; // Wifi name
+const char *password = "nofreewifi";// Wifi password
 // const char *ssid     = "UIT Public";
 // const char *password = "";
-
-WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "vn.pool.ntp.org", 25200, 60000); // ntpUDP, "VN", UTC7(in second), time update interval
-char weekDay [7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-String realTime, censor1;
-
-DHT dht(DHTPIN, DHTTYPE);
-float humid, temp, hic;
-
+char weekDay [7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; // Weekday format
+String realTime; // Time format
 
 void Initial() {
   //Initial necessary components
+  WiFi.begin(ssid, password); // Connect to wifi
 
-  WiFi.begin(ssid, password);
-
-  while ( WiFi.status() != WL_CONNECTED ) {
+  while ( WiFi.status() != WL_CONNECTED ) { // Waiting for wifi connection
     delay ( 500 );
     Serial.print ( "." );
   }
   Serial.println("");
 
   //Start components
-  timeClient.begin();
-  dht.begin();
-  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password);
+  timeClient.begin(); // NTPTimeClient start
+  dht.begin();// DHT11 start
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password); // Connect Blynk server
 
-  timer.setInterval(1000L, sendData);
+  timer.setInterval(1000L, sendData); // Delay time for sending information to Blynk server
 }
 
 void getData() {
   // Get data from components
   timeClient.update(); // Update time
-  Blynk.run();
+  Blynk.run();// Run Blynk
 
   String day = String(weekDay[timeClient.getDay()]);
   realTime = day + ' ' + timeClient.getFormattedTime() + '|'; // Formated datetime
+  humid = dht.readHumidity(); // Read humid
+  temp = dht.readTemperature(); // Read temperature as Celsius (the default)
 
-  // Read humid
-  humid = dht.readHumidity(); 
-  // Read temperature as Celsius (the default)
-  temp = dht.readTemperature();
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(humid) || isnan(temp)) {
+  if (isnan(humid) || isnan(temp)) { // Check if any reads failed and exit early (to try again).
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
-  // Compute heat index in Celsius (isFahreheit = false)
-  hic = dht.computeHeatIndex(temp, humid, false);
+  hic = dht.computeHeatIndex(temp, humid, false); // Compute heat index in Celsius (isFahreheit = false)
+  float rzero = mq135_sensor.getRZero();
+  correctedRZero = mq135_sensor.getCorrectedRZero(temp, humid); // Calculate RZero value
+  resistance = mq135_sensor.getResistance(); // Resistance
+  float ppm = mq135_sensor.getPPM();
+  correctedPPM = mq135_sensor.getCorrectedPPM(temp, humid); // Calcualte PPM value
 }
 
 void printData() {
@@ -81,17 +80,23 @@ void printData() {
   Serial.print(F("Heat index: "));
   Serial.print(hic);
   Serial.print(F("°C "));
+  Serial.print("Corrected RZero: ");
+  Serial.print(correctedRZero);
+  Serial.print(" Resistance: ");
+  Serial.print(resistance);
+  Serial.print(" Corrected PPM: ");
+  Serial.print(correctedPPM);
+  Serial.println("ppm");
   Serial.println("");
 }
 
 void sendData() {
   //Send data to Blynk
   // Serial.println("Sent to Blynk!");
-  Blynk.virtualWrite(V0, temp);
-  Blynk.virtualWrite(V1, humid);
+  Blynk.virtualWrite(V0, temp); // Virtual pin 0, tempurature
+  Blynk.virtualWrite(V1, humid); // Virtual pin 1, humid
   Blynk.virtualWrite(V2, realTime);
 }
-
 
 void setup() {
   // put your setup code here, to run once:
